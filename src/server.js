9e8f81bdb43561c3df196a050a99497bee0c2e44
Ilham Setiyo Kantomo <ilhamsetiyokantomo@gmail.com>
2023-08-10 03:53:29 +0700

    uploads implementation

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-underscore-dangle */
 // eslint-disable-next-line import/no-extraneous-dependencies
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const path = require('path');
+const Inert = require('@hapi/inert');
 
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
@@ -25,11 +28,16 @@ const _exports = require('./api/exports');
 const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
 
+const uploads = require('./api/uploads');
+const StorageService = require('./services/storage/StorageService');
+const UploadValidator = require('./validator/uploads');
+
 const init = async () => {
   const collaborationsService = new CollaborationsService();
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
 
   const server = Hapi.server({
         port: process.env.PORT,
@@ -44,6 +52,9 @@ const init = async () => {
     await server.register([
       {
         plugin: Jwt,
+      },
+      {
+        plugin: Inert,
       },
     ]);
 
@@ -100,6 +111,13 @@ const init = async () => {
         options: {
           service: ProducerService,
           validator: ExportsValidator,
+        },
+      },
+      {
+        plugin: uploads,
+        options: {
+          service: storageService,
+          validator: UploadValidator,
         },
       },
     ]);
